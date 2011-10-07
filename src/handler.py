a0755cc67c62862ae6534849f0c38dc2806ea9a0
Smotko <ensmotko@gmail.com>
2011-10-07 12:47:36 +0200

    Some code cleanup

--- src/handler.py
+++ src/handler.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import socket, asyncore, asynchat, random
+import socket, asyncore, asynchat
  
 class Bot( asynchat.async_chat ):
-    def __init__( self, nick, name, debug = True ):
+    def __init__( self, nick, name, channel, debug = True):
         asynchat.async_chat.__init__( self )
         self.buffer = ''
         self.set_terminator( '\r\n' )
-        self.nick = nick + str(random.random())[4:7]
+        self.nick = nick
         self.realname = name
+        self.channel = channel
         self.ident = None
         self.debug = debug
  
     def write( self, text ):
-        if self.debug == 1:
+        if self.debug:
             print '>> %s' % text
-        self.push( text + '\r\n' )
- 
+        self.push( text + '\r\n' ) 
  
     def handle_connect( self ):
         self.write( 'NICK %s' % self.nick )
@@ -24,23 +24,20 @@ class Bot( asynchat.async_chat ):
  
     def collect_incoming_data( self, data ):
         self.buffer += data
-
  
-    def found_terminator(self ):
+    def found_terminator(self):
         line= self.buffer
         self.buffer = ''
-        if self.debug == 1:
-            print '> %s' % line
+        if self.debug:
+            print '%s' % line
         if line.find('End of /MOTD command.') > 0:
-            self.write('JOIN #smotko-testing')
-            
-        if line.find('PING') > -1:
+            self.write('JOIN %s' % self.channel)
+        if line.startswith('PING'):
             self.write('PONG')
-        
  
-    def run( self, host, port ):
-        self.create_socket( socket.AF_INET, socket.SOCK_STREAM )
-        self.connect( ( host, port ) )
+    def run(self, host, port):
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
         asyncore.loop()
  
 
--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
+#!/usr/bin/python
 '''
 Created on Oct 7, 2011
 
 @author: smotko
 '''
 
-
-
 if __name__ == '__main__':
     from handler import Bot
     
     # initialize
-    mypybot = Bot( 'botko', 'botko')
+    mypybot = Bot('botko', 'botko', '#psywerx')
      
     # and run
-    mypybot.run( 'irc.freenode.org', 6667 )
+    mypybot.run('irc.freenode.org', 6667)
