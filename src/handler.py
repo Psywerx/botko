57f5758bc98527c0091a1e0cf73293e66ccacf67
Smotko <ensmotko@gmail.com>
2011-10-07 12:03:59 +0200

    Basic bot now working

--- src/handler.py
+++ src/handler.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
-import socket, asyncore, asynchat
+import socket, asyncore, asynchat, random
  
 class Bot( asynchat.async_chat ):
     def __init__( self, nick, name, debug = True ):
         asynchat.async_chat.__init__( self )
         self.buffer = ''
         self.set_terminator( '\r\n' )
-        self.nick = nick
+        self.nick = nick + str(random.random())[4:7]
         self.realname = name
+        self.ident = None
         self.debug = debug
  
     def write( self, text ):
         if self.debug == 1:
-            print '<< DEBUG >> WRITING "%s" TO SOCKET!' % text
+            print '>> %s' % text
         self.push( text + '\r\n' )
  
  
@@ -29,7 +30,13 @@ class Bot( asynchat.async_chat ):
         line= self.buffer
         self.buffer = ''
         if self.debug == 1:
-            print '<< DEBUG >> %s' % line
+            print '> %s' % line
+        if line.find('End of /MOTD command.') > 0:
+            self.write('JOIN #smotko-testing')
+            
+        if line.find('PING') > -1:
+            self.write('PONG')
+        
  
     def run( self, host, port ):
         self.create_socket( socket.AF_INET, socket.SOCK_STREAM )
