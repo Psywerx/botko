b049b12eaa001b5a3026f8c1676a549a4489a798
Smotko <ensmotko@gmail.com>
2011-10-07 10:07:56 +0200

    Added handler class

---
+++ src/handler.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 7, 2011
+
+@author: smotko
+'''
+
+class irc_handler(asynchat.async_chat):
+    
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
