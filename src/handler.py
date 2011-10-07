bb0b82e7a969d6818f3c2bdf30f40705a7ff5e2b
Smotko <ensmotko@gmail.com>
2011-10-07 10:35:38 +0200

    Made a basic IRC bot

--- src/handler.py
+++ src/handler.py
@@ -1,14 +1,40 @@
-'''
-Created on Oct 7, 2011
+#!/usr/bin/env python
 
-@author: smotko
-'''
+import socket, asyncore, asynchat
+ 
+class Bot( asynchat.async_chat ):
+    def __init__( self, nick, name, debug = True ):
+        asynchat.async_chat.__init__( self )
+        self.buffer = ''
+        self.set_terminator( '\r\n' )
+        self.nick = nick
+        self.realname = name
+        self.debug = debug
+ 
+    def write( self, text ):
+        if self.debug == 1:
+            print '<< DEBUG >> WRITING "%s" TO SOCKET!' % text
+        self.push( text + '\r\n' )
+ 
+ 
+    def handle_connect( self ):
+        self.write( 'NICK %s' % self.nick )
+        self.write( 'USER %s iw 0 :%s' % ( self.ident, self.realname ))
+ 
+    def collect_incoming_data( self, data ):
+        self.buffer += data
 
-class irc_handler(asynchat.async_chat):
-    
+ 
+    def found_terminator(self ):
+        line= self.buffer
+        self.buffer = ''
+        if self.debug == 1:
+            print '<< DEBUG >> %s' % line
+ 
+    def run( self, host, port ):
+        self.create_socket( socket.AF_INET, socket.SOCK_STREAM )
+        self.connect( ( host, port ) )
+        asyncore.loop()
+ 
 
-    def __init__(selfparams):
-        '''
-        Constructor
-        '''
-        
+ 
--- src/main.py
+++ src/main.py
@@ -4,5 +4,13 @@ Created on Oct 7, 2011
 @author: smotko
 '''
 
+
+
 if __name__ == '__main__':
-    print "Hello"
+    from handler import Bot
+    
+    # initialize
+    mypybot = Bot( 'botko', 'botko')
+     
+    # and run
+    mypybot.run( 'irc.freenode.org', 6667 )
