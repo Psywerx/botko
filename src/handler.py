44020f081425cd5ba0fe4dac31ce7287754b2bb7
Smotko <ensmotko@gmail.com>
2011-10-08 23:52:09 +0200

    Basic logging implemented

--- src/handler.py
+++ src/handler.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import socket, asyncore, asynchat
+from urllib import urlopen, urlencode
  
 class Bot( asynchat.async_chat ):
+    
     def __init__( self, nick, name, channel, debug = True):
         asynchat.async_chat.__init__( self )
         self.buffer = ''
@@ -12,6 +14,7 @@ class Bot( asynchat.async_chat ):
         self.channel = channel
         self.ident = None
         self.debug = debug
+        self.joined_channel = False
  
     def write( self, text ):
         if self.debug:
@@ -30,10 +33,23 @@ class Bot( asynchat.async_chat ):
         self.buffer = ''
         if self.debug:
             print '%s' % line
+            
+        # join desired channel:
         if line.find('End of /MOTD command.') > 0:
             self.write('JOIN %s' % self.channel)
-        if line.startswith('PING'):
+            # I know I should be checking if the JOIN command actually worked...
+            self.joined_channel = True
+        
+        # respond to pings:
+        if self.joined_channel and line.startswith('PING'):
             self.write('PONG')
+            
+        # if it isn't a ping request LOG IT:
+        else:
+            params = urlencode({'raw': line})
+            f = urlopen('http://192.168.1.102/irc:8080', params)
+            if f.read() != 'OK':
+                print "ERROR @ %s \n %s", line, f.read()
  
     def run(self, host, port):
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
