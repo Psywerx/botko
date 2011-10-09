eea9af9e8f0ec1c807ffdc300769c574f21f3145
Smotko <ensmotko@gmail.com>
2011-10-09 17:56:41 +0200

    Added settings file

--- src/handler.py
+++ src/handler.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python
 
 import socket, asyncore, asynchat
+import settings
 from urllib import urlopen, urlencode
+
  
 class Bot( asynchat.async_chat ):
     
-    def __init__( self, nick, name, channel, debug = True):
+    def __init__(self, debug = True):
         asynchat.async_chat.__init__( self )
         self.buffer = ''
         self.set_terminator( '\r\n' )
-        self.nick = nick
-        self.realname = name
-        self.channel = channel
+        self.nick = settings.BOT_NICK
+        self.realname = settings.BOT_NAME
+        self.channel = settings.CHANNEL
         self.ident = None
         self.debug = debug
         self.joined_channel = False
  
-    def write( self, text ):
+    def write(self, text):
         if self.debug:
             print '>> %s' % text
         self.push( text + '\r\n' ) 
  
-    def handle_connect( self ):
-        self.write( 'NICK %s' % self.nick )
-        self.write( 'USER %s iw 0 :%s' % ( self.ident, self.realname ))
+    def handle_connect(self):
+        self.write('NICK %s' % self.nick)
+        self.write('USER %s iw 0 :%s' % (self.ident, self.realname))
  
-    def collect_incoming_data( self, data ):
+    def collect_incoming_data(self, data):
         self.buffer += data
  
     def found_terminator(self):
@@ -48,16 +50,15 @@ class Bot( asynchat.async_chat ):
             
         # if it isn't a ping request LOG IT:
         elif self.joined_channel:
-            params = urlencode({'raw': line})
-            f = urlopen('http://192.168.1.102:8080/irc/', params)
+     
+            params = urlencode({'raw': line, 'token': settings.TOKEN})
+            f = urlopen(settings.SERVER_URL, params)
             response = f.read()
             if response  != 'OK':
-                print "ERROR @ %s" % f.read()
+                print "ERROR @ %s" % response
  
     def run(self, host, port):
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, port))
         asyncore.loop()
- 
-
  
---
+++ src/settings.py
@@ -0,0 +1,10 @@
+'''
+Created on Oct 9, 2011
+
+@author: smotko
+'''
+TOKEN        = ""
+SERVER_URL   = ""
+BOT_NICK     = ""
+BOT_NAME     = ""
+CHANNEL      = ""
