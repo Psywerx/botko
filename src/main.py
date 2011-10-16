30c46304a48cdffc62095aad8fb50507bdc41f0f
Smotko <ensmotko@gmail.com>
2011-10-17 00:53:28 +0200

    Restart on error & error log

--- src/main.py
+++ src/main.py
@@ -5,11 +5,28 @@ Created on Oct 7, 2011
 @author: smotko
 '''
 
+from traceback import format_exc
+from datetime import datetime
+from time import sleep
+
 if __name__ == '__main__':
     from handler import Bot
     
-    # initialize
-    mypybot = Bot()
-     
-    # and run
-    mypybot.run('irc.freenode.org', 6667)
+    while True:
+            
+        try:
+            
+            # initialize
+            mypybot = Bot()
+             
+            # and run
+            mypybot.run('irc.freenode.org', 6667)
+            
+        except Exception as e:
+            
+            f = open('error_log', 'a')
+            f.write(str(datetime.now()) + "\n")
+            f.write(str(format_exc() + "\n\n"))
+            f.close()
+            sleep(1)
+            
