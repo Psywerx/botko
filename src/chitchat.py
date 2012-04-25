6eeda431338351a296308caa85fe1e43fda95d2c
Swizec Teller <swizec@swizec.com>
2012-04-25 03:01:39 +0200

    buffer updates together since most are very short

--- src/chitchat.py
+++ src/chitchat.py
@@ -11,20 +11,26 @@ def take(n, iterable):
     return list(islice(iterable, n))
 
 def talk(n):
+    CORPUS.reset()
     acc = take(n, CORPUS)
 
     return " ".join([" ".join(ngram) for ngram in acc])
 
+BUFFER = []
 
 def chitchat(line):
     if "PRIVMSG" not in line:
         return False, ""
 
-    _, meta, message = line.split(':')
-    CORPUS.add(message)
+    _, meta, message = line.split(':', 2)
+    BUFFER.append(message)
 
-    print talk(5)
-    print ""
+    if len(BUFFER) >= 5: # 5 felt like a good enough number of messages to treat as a unit
+        CORPUS.add(" ".join(BUFFER))
+        del BUFFER[:]
+
+        print talk(5)
+        print ""
 
 
     return False, ""
