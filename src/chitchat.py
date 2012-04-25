090c117bb1f048e664158c2f75244af3588948cb
Swizec Teller <swizec@swizec.com>
2012-04-25 19:42:46 +0200

    refactored velocity_rate

--- src/chitchat.py
+++ src/chitchat.py
@@ -14,29 +14,29 @@ def take(n, iterable):
 
 class Chatty(object):
     # how many recent messages to consider in stuff
-    MSGS = 5
+    messages = 5
 
     # used when deciding whether to speak
-    CHATTINESS_LONG = 30
-    CHATTINESS_SHORT = 5
+    window_big = 60
+    window_small = 10
 
     def __init__(self):
         self.buffer = []
         self.corpus = Corpus()
-        self.avg_len = range(self.MSGS)
+        self.avg_len = range(self.messages)
         self.counter = 0
         self.recent_timestamps = range(self.CHATTINESS_LONG)
 
     def chat(self, line):
-        if "PRIVMSG" not in line:
+        if "PRIVMSG "+settings.CHANNEL not in line:
             return False, ""
 
         _, meta, message = line.split(':', 2)
 
         self.buffer.append(message)
-        self.avg_len[self.counter%self.MSGS] = len(message.split(" "))
+        self.avg_len[self.counter%self.messages] = len(message.split(" "))
 
-        if len(self.buffer) >= self.MSGS:
+        if len(self.buffer) >= self.messages:
             self.corpus.add(" ".join(self.buffer))
             del self.buffer[:]
 
@@ -49,11 +49,11 @@ class Chatty(object):
         return speak, msg
 
     def talk(self):
-        if self.counter < self.MSGS:
+        if self.counter < self.messages:
             return "I'm just admiring the shape of your skull"
 
         self.corpus.rewind()
-        line = take(ceil(sum(self.avg_len)/self.MSGS),
+        line = take(ceil(sum(self.avg_len)/self.messages),
                     self.corpus)
 
         return " ".join([" ".join(ngram) for ngram in line])
@@ -62,6 +62,7 @@ class Chatty(object):
         # starting conditions, we don't have enough timestamps :(
         if self.counter < self.CHATTINESS_LONG:
             return False
+
 
         # talk more when heated debate is happening
         now = int(time())
@@ -92,11 +93,29 @@ class Chatty(object):
         # add new timestamp
         self.recent_timestamps[i] = now
 
-        # ratio between speed of last <10> messages and speed of last <60>
-        # determines probability of speaking
-        speed = float(now-oldest_10)/float(now-oldest)
 
-        return random.random() < max(accel, speed)
+        return random.random() < max(self.accel_rate(),
+                                     self.velocity_rate())
+
+    def velocity_rate(self):
+        # the oldest timestamp is exactly opposite current in the ist
+        oldest = self.timestamps(-self.window_big+1)
+        oldest_10 = self.timestamps(-self.window_big+10)
+
+        now = int(time())
+
+        return float(now-oldest_10)/float(now-oldest)
+
+    def timestamps(self, start, end = None):
+        cur = self.counter%self.window_big
+
+        start = (cur+start)%self.window_big
+
+        if end == None:
+            return self.recent_timestamps[start]
+
+        end = (cur+end)%self.window_big
+        return self.recent_timestamps[start:end]
 
     def mentioned(self, msg):
         return settings.BOT_NICK in msg
