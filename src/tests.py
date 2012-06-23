071331a73c0038ee04d13667998ff9c6b2e9931b
Smotko <ensmotko@gmail.com>
2012-06-23 22:44:06 +0200

    Fixed unit tests

--- src/tests.py
+++ src/tests.py
@@ -3,23 +3,26 @@ Created on May 10, 2012
 
 @author: smotko
 '''
-import unittest
 from handler import Bot
+from logic import BotLogic
+
+import unittest
 
 class BotTest(unittest.TestCase):
     
     def setUp(self):
         self.bot = Bot()
+        self.logic = BotLogic(self.bot)
 
     def testParsePrivMsg(self):
         line = ":HairyFotr!~Matic@89-212-218-130.dynamic.t-2.net PRIVMSG #psywerx :I will try to improve my tunapasta from this data :P"
-        nick, msg = self.bot.parse(line)
+        nick, msg = self.logic.parse_msg(line)
         self.assertEqual("HairyFotr", nick, "Nick does not match")
         self.assertEqual("I will try to improve my tunapasta from this data :P", msg, "Msg does not match")
 
     def testParseEvent(self):
         line = ":smotko!~smotko@cpe-212-85-162-22.cable.telemach.net QUIT :Quit: smotko"
-        nick, msg = self.bot.parse(line)
+        nick, msg = self.logic.parse_msg(line)
         self.assertEqual("smotko", nick)
         self.assertEqual("Quit: smotko", msg)
 
