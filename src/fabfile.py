18f731a8b4a45a11651afa972924474d36c78577
Smotko <ensmotko@gmail.com>
2012-09-12 16:10:04 +0200

    Added a fabfile for easier updating

---
+++ src/fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import *
+
+env.use_ssh_config = True
+
+env.hosts = ['server']
+
+def host():
+    run('uname -a')
+
+def update():
+    with cd('botko'):
+        out = run('git pull')
+        if 'Already up-to-date.' not in out:
+            run('botko restart')
