6e42c9bc08ae5cceb1db141df9a37b8c402ec9f5
Smotko <ensmotko@gmail.com>
2012-09-16 01:48:53 +0200

    Added a nondaemon mode

--- src/main.py
+++ src/main.py
@@ -3,29 +3,32 @@
 import sys, time
 from daemon import Daemon
 
+def run_bot():
+    import settings
+    from handler import Bot
+    
+    while True:
+        try:
+            # initialize
+            botko = Bot()
+            # and run
+            botko.run(settings.IRC_SERVER, settings.IRC_PORT)
+            
+        except Exception as e:
+            # log the error
+            from traceback import format_exc
+            from datetime import datetime
+            from time import sleep
+            
+            f = open('error_log', 'a')
+            f.write(str(datetime.now()) + "\n")
+            f.write(str(format_exc() + "\n\n"))
+            f.close()
+            sleep(10)
+
 class BotkoDaemon(Daemon):
     def run(self):
-        import settings
-        from handler import Bot
-        
-        while True:
-            try:
-                # initialize
-                botko = Bot()
-                # and run
-                botko.run(settings.IRC_SERVER, settings.IRC_PORT)
-                
-            except Exception as e:
-                # log the error
-                from traceback import format_exc
-                from datetime import datetime
-                from time import sleep
-                
-                f = open('error_log', 'a')
-                f.write(str(datetime.now()) + "\n")
-                f.write(str(format_exc() + "\n\n"))
-                f.close()
-                sleep(10)
+        run_bot()
 
 if __name__ == "__main__":
     daemon = BotkoDaemon('/tmp/daemon-example.pid')
@@ -36,6 +39,8 @@ if __name__ == "__main__":
             daemon.stop()
         elif 'restart' == sys.argv[1]:
             daemon.restart()
+        elif 'nodaemon' == sys.argv[1]:
+            run_bot()
         else:
             print "Unknown command"
             sys.exit(2)
