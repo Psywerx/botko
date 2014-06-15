a08c69d20df8a540a8cbfd7e49bc9eeafa526cbb
Anže Pečar <ensmotko@gmail.com>
2014-06-15 08:56:43 -0700

    Better responses for youtube links

--- src/plugins/read_links.py
+++ src/plugins/read_links.py
@@ -1,12 +1,23 @@
 from base import BotPlugin
 from tweepy import OAuthHandler, API
 from settings import TWITTER as t
+from response import random_response
 import re
 oauth = OAuthHandler(t['consumer_key'], t['consumer_secret'])
 oauth.set_access_token(t['access_token_key'], t['access_token_secret'])
 twt = API(oauth)
 twt_regex = re.compile("https?://(?:www\\.)?twitter\\.com/.*/status(?:es)?/([0-9]+).*")
 
+YOUTUBE_RESPONSES = [
+    "That video is titled '%(title)s'. You will waste %(seconds)ss of your life watching it.",
+    "The title of that yt video is '%(title)s'. It has been viewed %(views)s times.",
+    "Title: '%(title)s', Views: '%(views)s', duration: %(seconds)ss.",
+    "Title of that yt video is '%(title)s'.",
+    "Yt video titled '%(title)s' and it has an avarage rating of %(rating)s.",
+    "Here is the title of that yt video: '%(title)s'.",
+    "I found the title of that yt video, here it is: '%(title)s'",
+    "If you click that link you will watch a video titled '%(title)s'. Good luck!"
+]
 
 class ReadLinks(BotPlugin):
 
@@ -15,16 +26,14 @@ class ReadLinks(BotPlugin):
         if not res:
             return
         try:
-            st = twt.get_status(str(res.groups()[0])).text.replace('\n', ' ')
-            name = st.user.screen_name
-
-            response = unicode("@" + name + " on Twitter says: " + st)
+            status = twt.get_status(str(res.groups()[0]))
+            name = status.user.screen_name
+            text = status.text.replace('\n', ' ')
+            response = unicode("@" + name + " on Twitter says: " + text)
             response = response.encode('utf8')
             self.bot.say(response, channel)
-        except Exception, e:
-            self.bot.say(
-                'Sorry, I wasn\'t able to read the last tweet :('
-                + str(e), channel)
+        except Exception:
+            self.bot.say('Sorry, I wasn\'t able to read the last tweet :(', channel)
 
     def _read_youtube(self, channel, msg):
         if 'youtu.be' not in msg and 'youtube.com' not in msg:
@@ -37,7 +46,13 @@ class ReadLinks(BotPlugin):
             from gdata.youtube import service
             client = service.YouTubeService()
             video = client.GetYouTubeVideoEntry(video_id=video_id)
-            self.bot.say('Title of that yt video is \'' + video.title.text + '\'', channel)
+            video_info = {
+                'title': video.title.text,
+                'seconds': video.media.duration.seconds,
+                'views': video.statistics.view_count,
+                'rating': video.rating.average,
+            }
+            self.bot.say(random_response(YOUTUBE_RESPONSES) % video_info, channel)
         except:
             self.bot.say('For some reason I couldn\'t read the title of that yt link.')
 
