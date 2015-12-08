5a0643ea4ab8c6e4300afc151fed4b5b5b0c39e6
HairyFotr <hairyfotr@gmail.com>
2015-12-08 20:06:24 +0100

    Camel to that_thing

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -15,7 +15,7 @@ class BotTest(unittest.TestCase):
         self.bot = Bot()
         self.logic = BotLogic(self.bot)
 
-    def test_ParsePrivMsg(self):
+    def test_parse_priv_msg(self):
         line = ":HairyFotr!~Matic@89-212-218-130.dynamic.t-2.net " \
             + "PRIVMSG #psywerx :I will try to improve my tunapasta " \
             + "from this data :P"
@@ -24,14 +24,14 @@ class BotTest(unittest.TestCase):
         self.assertEqual("I will try to improve my tunapasta "
                          + "from this data :P", msg, "Msg does not match")
 
-    def test_ParseEvent(self):
+    def test_parse_event(self):
         line = ":smotko!~smotko@cpe-212-85-162-22.cable.telemach.net QUIT " \
             + ":Quit: smotko"
         nick, msg, _ = self.logic.parse_msg(line)
         self.assertEqual("smotko", nick)
         self.assertEqual("Quit: smotko", msg)
 
-    def test_ParseWeird(self):
+    def test_parse_weird(self):
         line = ":smotko!~smotko@2001:1470:fffe:fe01:4c8b:3839:ad5f:3bbb " \
             + "JOIN #smotko-testing"
         nick, msg, channel = self.logic.parse_msg(line)
